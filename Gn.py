958d9e2f92cea1346eac1a80f15e7996b3f22ee8
D11216209 <d11216209@gs.takming.edu.tw>
2025-11-14 16:11:18 +0800

    Update Gn.py

--- Gn.py
+++ Gn.py
@@ -1,6 +1,5 @@
 
 #猜數字遊戲 •	玩家每次輸入猜測數字，程式提示「大了」或「小了」。答對時顯示「恭喜通過！」並結束遊戲。程式需記錄猜測次數，顯示學員的持續進步。
-
 import random
 def guess_number_game():
     number_to_guess = random.randint(1, 100)
@@ -21,4 +20,4 @@ def guess_number_game():
                 break
         except ValueError:
             print("請輸入一個有效的整數。")
-            
+guess_number_game()
